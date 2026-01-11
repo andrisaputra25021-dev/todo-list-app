58dae72e0375fa3c719f3108062c73ef60e3aea9
Andri Saputra <andri.saputra25021@gmail.com>
2026-01-11 23:55:56 +0700

    update: add submit form logic on main.js

--- main.js
+++ main.js
@@ -1,27 +1,70 @@
 // setting waktu otomatis
 const dateElement = document.getElementById("date");
+const timeElement = document.getElementById("time");
 
 function timeUpdate() {
-  const today = new Date();
-  const timeDetail = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
+  const now = new Date();
+
+  const dateFormat = {
     day: "numeric",
+    weekday: "long",
+    month: "long",
+    year: "numeric",
+  };
+
+  const timeFormat = {
     hour: "2-digit",
     minute: "2-digit",
   };
 
-  dateElement.innerText = today.toLocaleDateString("id-ID", timeDetail);
+  dateElement.innerText = now.toLocaleDateString("id-ID", dateFormat);
+  timeElement.innerText = now.toLocaleTimeString("id-ID", timeFormat);
 }
+
 timeUpdate();
 setInterval(timeUpdate, 1000);
 
-// sumber data utma
-let taskList = [];
+// sumber data utama
+let data = [];
 
 // load data dari localstorage
-const savedTasks = localStorage.getItem("tasks");
+const savedTasks = localStorage.getItem("taskList");
 if (savedTasks) {
-  taskList = JSON.parse(savedTasks);
+  data = JSON.parse(savedTasks);
 }
+
+// proses submit form
+const form = document.getElementById("task-form");
+const taskInput = document.getElementById("task-input");
+const taskDate = document.getElementById("task-date");
+const taskTime = document.getElementById("task-time");
+const taskPriorty = document.getElementById("task-priority");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const text = taskInput.value;
+  const date = taskDate.value;
+  const time = taskTime.value;
+  const priority = taskPriorty.value;
+
+  if (!text || !date || !time || !priority) {
+    alert("Semua field harus diisi!");
+    return;
+  }
+
+  const newTask = {
+    id: Date.now(),
+    text: text,
+    date: date,
+    time: time,
+    priority: priority,
+    complete: false,
+  };
+
+  data.push(newTask);
+
+  localStorage("taskList", JSON.stringify(data));
+
+  form.reset();
+});
